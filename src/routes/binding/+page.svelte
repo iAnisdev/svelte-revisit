84daa74711463dd587232375242543d157942103
Anis <imarahmandev@gmail.com>
2024-03-10 23:45:59 +0700

    Binding

---
+++ src/routes/binding/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import BindGroup from '../../component/BindGroup.svelte';
+	let name: string = '';
+	let age: number = 25;
+</script>
+
+<input bind:value={name} placeholder="Enter your name" />
+<p>Hello {name}!</p>
+<input type="number" bind:value={age} placeholder="Enter your age" />
+<p>{name} are {age} years old.</p>
+
+<BindGroup />
